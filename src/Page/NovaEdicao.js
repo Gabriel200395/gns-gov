f07e2afb5df6448f296aec7e13305c430c304cd0
Gabriel Souza <eugabrielf@gmail.com>
2021-03-03 18:24:26 -0300

    criando componentes

--- src/Page/NovaEdicao.js
+++ src/Page/NovaEdicao.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Menus from "../Components/Menus/Menus";
 import Navbar from "../Components/Navbar/Navbar";
+import MenuNovaEdicao from "../Components/MenuNovaEdicao/MenuNovaEdicao";
 
 function NovaEdicao() {
   return (
     <section>
       <Navbar />
       <Menus />
+      <MenuNovaEdicao />
     </section>
   );
 }
