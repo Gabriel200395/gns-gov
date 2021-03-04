53e5e62eeae23a596adb782b4f3adc83df6ae3de
Gabriel Souza <eugabrielf@gmail.com>
2021-03-04 16:25:17 -0300

    criando pagina de Login

---
+++ src/Page/Login.js
@@ -0,0 +1,19 @@
+import Footer from "../Components/Footer/Footer";
+import SubRodape from "../Components/SubRodape/SubRodape";
+import Informacao from "../Components/Informacao/Informacao";
+import NavbarLogin from "../Components/ComponentsLogin/NavbarLogin/NavbarLogin";
+import Formulario from "../Components/ComponentsLogin/Formulario/Formulario";
+
+function Login() {
+  return (
+    <section>
+      <NavbarLogin />
+      <Formulario />
+      <Informacao />
+      <Footer />
+      <SubRodape />
+    </section>
+  );
+}
+
+export default Login;
