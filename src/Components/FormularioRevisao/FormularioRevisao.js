bd0d7d831caf7dc6f3a837f6c9d62a8f7e214983
Gabriel Souza <eugabrielf@gmail.com>
2021-03-04 14:37:52 -0300

    terminando Segunda Pagina

--- src/Components/FormularioRevisao/FormularioRevisao.js
+++ src/Components/FormularioRevisao/FormularioRevisao.js
@@ -1,23 +1,24 @@
 import TextoContainer from "../ComponetsForm/TextoContainer";
 import InputLarge from "../ComponetsForm/InputLarge";
 import Button from "../ComponetsForm/Button";
+import "./formulario-revisao.css";
 
 function FormularioRevisao() {
   return (
-    <div className="container" style={{ marginBottom: "60px" }}>
+    <div className="container formulario-revisao">
       <div className="row">
         <div className="col-12">
           <form>
             <div className="row">
               <div className="col-12">
-                <TextoContainer titulo="Quantidade prevista de páginas" />
+                <TextoContainer titulo="Data de publicação" />
                 <div className="row">
                   <InputLarge type="date" />
                 </div>
               </div>
 
               <div className="col-12">
-                <TextoContainer titulo="Definir automaticamente numeração nas páginas?" />
+                <TextoContainer titulo="Definir como edição modelo?" />
                 <div className="row">
                   <Button des="sim" />
                   <Button des="não" />
@@ -25,7 +26,7 @@ function FormularioRevisao() {
               </div>
 
               <div className="col-12">
-                <TextoContainer titulo="Quantidade prevista de páginas" />
+                <TextoContainer titulo="Nome da edição" />
                 <div className="row">
                   <InputLarge placeholder="Modelo exemplo" type="text" />
                 </div>
