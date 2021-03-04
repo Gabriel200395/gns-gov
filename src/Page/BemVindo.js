fcf8198f9b21d7b6bb3dba8abdfd0f711296528f
Gabriel Souza <eugabrielf@gmail.com>
2021-03-04 17:01:12 -0300

    colocando submenus

--- src/Page/BemVindo.js
+++ src/Page/BemVindo.js
@@ -2,11 +2,14 @@ import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 import SubRodape from "../Components/SubRodape/SubRodape";
 import Informacao from "../Components/Informacao/Informacao";
+import Menus from "../Components/Menus/Menus";
 
 function BemVindo() {
   return (
     <section>
       <Navbar />
+      <Menus />
+
       <Informacao />
       <Footer />
       <SubRodape />
