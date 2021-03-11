d8960a98c224f0097a0cd70af6f294a304d929fa
Gabriel Souza <eugabrielf@gmail.com>
2021-03-11 12:09:21 -0300

    fazendo ajuste

--- src/Components/ComponetsForm/InputTextDate.js
+++ src/Components/ComponetsForm/InputTextDate.js
@@ -5,7 +5,6 @@ function InputTextDate({ placeholder, type, ...props }) {
     <div className="col-3 ">
       <input
         type={type}
-        name={type}
         placeholder={placeholder}
         className="form-control"
         {...props}
