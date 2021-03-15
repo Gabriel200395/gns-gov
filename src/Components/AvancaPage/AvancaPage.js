87f4c3ad5a89b9c216411de81e69f4133fe27cd8
Gabriel Souza <eugabrielf@gmail.com>
2021-03-15 10:32:21 -0300

    fazendo pequenas alteraçoes

--- src/Components/AvancaPage/AvancaPage.js
+++ src/Components/AvancaPage/AvancaPage.js
@@ -7,7 +7,9 @@ function AvancaPage() {
         <div className="row align-items-center menu-border-avanca-page">
           <div className="col-6 ps-0">
             <div className="d-flex rodape-avanca">
-              <button className="cancelar">Cancelar</button>
+              <Link to="/bem-vindo">
+                <button className="cancelar">Cancelar</button>
+              </Link>
             </div>
           </div>
           <div className="col-6 d-flex justify-content-end pe-0">
